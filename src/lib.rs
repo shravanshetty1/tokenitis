d99cafb4093c1fd9dad3f8bd386a3e13cf13c040
Shravan Shetty <shravanshetty322@gmail.com>
2022-05-15 06:03:43 +0530

    demo day hacks

--- src/lib.rs
+++ src/lib.rs
@@ -13,4 +13,4 @@ pub mod entrypoint;
 
 type Result<T> = core::result::Result<T, Box<dyn std::error::Error>>;
 
-solana_program::declare_id!("5fTuAJMQKJdXo5N1dZD47t4LC3VRzbfsBbpNXB6Z3XBA");
+solana_program::declare_id!("FyNEh6YBfJNKEExMDe27X7gDH3FXWp5oGAuMTCTYNqre");
